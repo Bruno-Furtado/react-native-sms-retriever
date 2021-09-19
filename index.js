884a4228007c5b5296a1a090f22fb4a4d2296891
Humaid Arif Khan <humaid.k@tlt.local>
2021-09-19 07:19:46 +0400

    mock functions to avoid crash in ios

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-import { DeviceEventEmitter, NativeModules, Platform } from 'react-native';
+import { DeviceEventEmitter, NativeModules, Platform } from "react-native";
 
 const { RNSmsRetrieverModule } = NativeModules;
-const EVEN_LISTENER = 'me.furtado.smsretriever:SmsEvent';
+const EVEN_LISTENER = "me.furtado.smsretriever:SmsEvent";
 
-const SmsRetrieverModule = (Platform.OS === "ios") ? null : {
-  requestPhoneNumber: RNSmsRetrieverModule.requestPhoneNumber,
-  startSmsRetriever: RNSmsRetrieverModule.startSmsRetriever,
-  addSmsListener: (callback) => DeviceEventEmitter.addListener(EVEN_LISTENER, callback),
-  removeSmsListener: () => DeviceEventEmitter.removeAllListeners(EVEN_LISTENER)
-};
+const SmsRetrieverModule =
+  Platform.OS === "ios"
+    ? {
+        requestPhoneNumber: () => null,
+        startSmsRetriever: () => null,
+        addSmsListener: () => null,
+        removeSmsListener: () => null,
+      }
+    : {
+        requestPhoneNumber: RNSmsRetrieverModule.requestPhoneNumber,
+        startSmsRetriever: RNSmsRetrieverModule.startSmsRetriever,
+        addSmsListener: (callback) =>
+          DeviceEventEmitter.addListener(EVEN_LISTENER, callback),
+        removeSmsListener: () =>
+          DeviceEventEmitter.removeAllListeners(EVEN_LISTENER),
+      };
 
 export default SmsRetrieverModule;
